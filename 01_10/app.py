310dcea3a734e82a4c63ccacd5235d824b838c11
Lincy <lincgr@gmail.com>
2024-02-19 17:52:56 -0600

    01_10 Gráfico circular con Matplotlib

---
+++ 01_10/app.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df_gastos = pd.DataFrame({
+        "categorías": ["Comida", "Vivienda", "Transporte", "Ropa", "Ahorro", "Otros"],
+        "gastos": [170, 280, 120, 100, 110, 220],
+    })
