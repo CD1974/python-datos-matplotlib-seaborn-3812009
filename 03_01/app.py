6003b0e36a774527a4db27157e5c5a4ee2516bef
Lincy <lincgr@gmail.com>
2024-02-21 20:27:07 -0600

    03_01 Gráficos segmentados (Faceting) con Seaborn

---
+++ 03_01/app.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df_ventas = pd.DataFrame({
+    'cromos': ['Cromo 101', 'Cromo 102', 'Cromo 101', 'Cromo 102', 'Cromo 101', 'Cromo 102', 'Cromo 101', 'Cromo 102'],
+    'trimestre': ['T1', 'T1',  'T2', 'T2',  'T3', 'T3', 'T4', 'T4'],
+    'ventas': [100, 150, 200, 123, 258, 156, 369, 212]
+  })
