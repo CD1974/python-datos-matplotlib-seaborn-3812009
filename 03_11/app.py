a41202c266d131c7cec291bd86ec1623113be64b
Lincy <lincgr@gmail.com>
2024-02-23 21:03:25 -0600

    03_11 Gráficos de 3D con Matplotlib

---
+++ 03_11/app.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df_ventas = pd.DataFrame({
+    'annio': [2019, 2020, 2021, 2022, 2023, 2024],
+    'precio_cromo': [150, 210, 250, 320, 350, 400],
+    'unidades': [12, 23, 20, 52, 45, 10]
+   })
